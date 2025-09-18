9e9e931c4540458b44a353cbf72d212071b28411
Владислав Плоткин <vlad@krasnodar-it-service.ru>
2025-09-18 17:39:53 +0300

    Поменял логику,  добавил stdin и stdout

--- cmd/merge/main.go
+++ cmd/merge/main.go
@@ -2,39 +2,122 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"game.com/m/internal/parser"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		fmt.Println("Использование: merge_events <базовый файл> <добавочный файл> <итоговый файл>")
+	// Проверяем количество аргументов
+	if len(os.Args) < 2 || len(os.Args) > 4 {
+		fmt.Println("Использование: merge_events [базовый файл] <добавочный файл> [итоговый файл]")
+		fmt.Println("  если базовый файл не указан или '-', читаем из stdin")
+		fmt.Println("  если итоговый файл не указан или '-', пишем в stdout")
 		os.Exit(1)
 	}
 
-	baseFile := os.Args[1]
-	addFile := os.Args[2]
-	outputFile := os.Args[3]
+	// Определяем аргументы
+	var baseFile, addFile, outputFile string
 
-	base, err := parser.ParseEvemuFile(baseFile)
+	if len(os.Args) == 2 {
+		baseFile = "-"
+		addFile = os.Args[1]
+		outputFile = "-"
+	} else if len(os.Args) == 3 {
+		baseFile = os.Args[1]
+		addFile = os.Args[2]
+		outputFile = "-"
+	} else {
+		baseFile = os.Args[1]
+		addFile = os.Args[2]
+		outputFile = os.Args[3]
+	}
+
+	// Чтение базового файла (или stdin)
+	var base *parser.EvemuFile
+	var err error
+
+	if baseFile == "-" {
+		base, err = readFromStdin()
+	} else {
+		base, err = parser.ParseEvemuFile(baseFile)
+	}
+
 	if err != nil {
 		fmt.Printf("Ошибка чтения базового файла: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Чтение добавочного файла (обязательно из файла)
 	add, err := parser.ParseEvemuFile(addFile)
 	if err != nil {
 		fmt.Printf("Ошибка чтения добавочного файла: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Объединение файлов
 	merged := base.Merge(add)
 
-	if err := merged.WriteToFile(outputFile); err != nil {
-		fmt.Printf("Ошибка записи файла: %v\n", err)
+	// Запись результата
+	if outputFile == "-" {
+		err = writeToStdout(merged)
+	} else {
+		err = merged.WriteToFile(outputFile)
+	}
+
+	if err != nil {
+		fmt.Printf("Ошибка записи: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Готово! Файлы объединены в %s\n", outputFile)
+	fmt.Fprintf(os.Stderr, "Готово! Файлы объединены в %s\n", outputFile)
+}
+
+// readFromStdin читает данные из stdin и парсит их как EvemuFile
+func readFromStdin() (*parser.EvemuFile, error) {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка чтения из stdin: %v", err)
+	}
+
+	// Создаем временный файл для парсинга
+	tmpfile, err := os.CreateTemp("", "stdin_input")
+	if err != nil {
+		return nil, fmt.Errorf("ошибка создания временного файла: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	if _, err := tmpfile.Write(data); err != nil {
+		return nil, fmt.Errorf("ошибка записи во временный файл: %v", err)
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		return nil, fmt.Errorf("ошибка закрытия временного файла: %v", err)
+	}
+
+	return parser.ParseEvemuFile(tmpfile.Name())
+}
+
+// writeToStdout записывает EvemuFile в stdout
+func writeToStdout(file *parser.EvemuFile) error {
+	// Создаем временный файл для записи
+	tmpfile, err := os.CreateTemp("", "stdout_output")
+	if err != nil {
+		return fmt.Errorf("ошибка создания временного файла: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	if err := file.WriteToFile(tmpfile.Name()); err != nil {
+		return fmt.Errorf("ошибка записи во временный файл: %v", err)
+	}
+
+	// Читаем и выводим содержимое
+	data, err := os.ReadFile(tmpfile.Name())
+	if err != nil {
+		return fmt.Errorf("ошибка чтения временного файла: %v", err)
+	}
+
+	fmt.Print(string(data))
+	return nil
 }
--- cmd/repeat/main.go
+++ cmd/repeat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 
@@ -9,31 +10,114 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		fmt.Println("Использование: repeat_events <входной файл> <количество повторов> <выходной файл>")
+	// Проверяем количество аргументов
+	if len(os.Args) < 2 || len(os.Args) > 4 {
+		fmt.Println("Использование: repeat_events [входной файл] <количество повторов> [выходной файл]")
+		fmt.Println("  если входной файл не указан или '-', читаем из stdin")
+		fmt.Println("  если выходной файл не указан или '-', пишем в stdout")
 		os.Exit(1)
 	}
 
-	inputFile := os.Args[1]
-	repeatCount, err := strconv.Atoi(os.Args[2])
+	// Определяем аргументы
+	var inputFile, outputFile string
+	var repeatCount int
+	var err error
+
+	if len(os.Args) == 2 {
+		inputFile = "-"
+		repeatCount, err = strconv.Atoi(os.Args[1])
+		outputFile = "-"
+	} else if len(os.Args) == 3 {
+		inputFile = os.Args[1]
+		repeatCount, err = strconv.Atoi(os.Args[2])
+		outputFile = "-"
+	} else {
+		inputFile = os.Args[1]
+		repeatCount, err = strconv.Atoi(os.Args[2])
+		outputFile = os.Args[3]
+	}
+
 	if err != nil {
 		fmt.Printf("Ошибка преобразования количества повторов: %v\n", err)
 		os.Exit(1)
 	}
-	outputFile := os.Args[3]
 
-	file, err := parser.ParseEvemuFile(inputFile)
+	// Чтение входного файла (или stdin)
+	var file *parser.EvemuFile
+
+	if inputFile == "-" {
+		file, err = readFromStdin()
+	} else {
+		file, err = parser.ParseEvemuFile(inputFile)
+	}
+
 	if err != nil {
 		fmt.Printf("Ошибка чтения файла: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Генерация повторений
 	repeated := file.GenerateRepeatedEvents(repeatCount)
 
-	if err := repeated.WriteToFile(outputFile); err != nil {
-		fmt.Printf("Ошибка записи файла: %v\n", err)
+	// Запись результата
+	if outputFile == "-" {
+		err = writeToStdout(repeated)
+	} else {
+		err = repeated.WriteToFile(outputFile)
+	}
+
+	if err != nil {
+		fmt.Printf("Ошибка записи: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Готово! Сгенерировано %d повторов в файле %s\n", repeatCount, outputFile)
+	fmt.Fprintf(os.Stderr, "Готово! Сгенерировано %d повторов в %s\n", repeatCount, outputFile)
+}
+
+// readFromStdin читает данные из stdin и парсит их как EvemuFile
+func readFromStdin() (*parser.EvemuFile, error) {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка чтения из stdin: %v", err)
+	}
+
+	// Создаем временный файл для парсинга
+	tmpfile, err := os.CreateTemp("", "stdin_input")
+	if err != nil {
+		return nil, fmt.Errorf("ошибка создания временного файла: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	if _, err := tmpfile.Write(data); err != nil {
+		return nil, fmt.Errorf("ошибка записи во временный файл: %v", err)
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		return nil, fmt.Errorf("ошибка закрытия временного файла: %v", err)
+	}
+
+	return parser.ParseEvemuFile(tmpfile.Name())
+}
+
+// writeToStdout записывает EvemuFile в stdout
+func writeToStdout(file *parser.EvemuFile) error {
+	// Создаем временный файл для записи
+	tmpfile, err := os.CreateTemp("", "stdout_output")
+	if err != nil {
+		return fmt.Errorf("ошибка создания временного файла: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	if err := file.WriteToFile(tmpfile.Name()); err != nil {
+		return fmt.Errorf("ошибка записи во временный файл: %v", err)
+	}
+
+	// Читаем и выводим содержимое
+	data, err := os.ReadFile(tmpfile.Name())
+	if err != nil {
+		return fmt.Errorf("ошибка чтения временного файла: %v", err)
+	}
+
+	fmt.Print(string(data))
+	return nil
 }
